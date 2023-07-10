568b9fb9146ce8d9dee931b5748cda5c1f2c3a5f
Konjohn <114719370+Konjohn@users.noreply.github.com>
2023-07-10 16:11:53 +0700

    Create Zz.cpp

---
+++ Zz.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int num;
+string letter;
+void Zz();
+int main() {
+    cout << "Enter number: ";
+    cin >> input;
+    cout << "Enter number: ";
+    cin >> letter;
+    Zz();
+    return 0;
+}
+void Zz() {
+    int length = letter.length();
+    
+    for (int i = 0; i < length; i++) {
+        letterArray[i] = letter[i];
+    }
+}
